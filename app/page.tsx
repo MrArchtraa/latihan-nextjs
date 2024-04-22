fe54c704d6eba1ec768cdf05afcc7d7a81b153f6
Andhika R. <archtraa.creative@gmail.com>
2024-04-22 20:36:27 +0700

    tes button

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,13 @@
 import Button from "./components/Buttons/Button";
-import OutlinedButton from "./components/Buttons/OutlinedButton";
-import CancelButton from "./components/Buttons/CancelButton";
-import OutlinedCancelButton from "./components/Buttons/OutlinedCancelButton";
-
-import Card from "./components/Cards/Card";
-import CardHeader from "./components/Cards/CardHeader";
-import CardBody from "./components/Cards/CardBody";
-import CardFooter from "./components/Cards/CardFooter";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Card>
-        <CardHeader>
-          <h3 className="text-lg font-bold">Simple Card</h3>
-        </CardHeader>
-        <CardBody>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a hendrerit ipsum.
-            Pellentesque consectetur odio diam, sit amet dapibus turpis dapibus non.
-            Curabitur laoreet euismod bibendum.
-          </p>
-        </CardBody>
-        <CardFooter>
-          <Button>
-            Press me
-          </Button>
-        </CardFooter>
-      </Card>
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Button>
+          <h1 className="text-xl">My Website</h1>
+        </Button>
+      </main>
+    </>
   );
 }
