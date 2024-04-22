2eaa1d0aa80708b37fdd723d4394f661040c6f2d
Andhika R. <archtraa.creative@gmail.com>
2024-04-22 21:58:04 +0700

    perbaikan komponen

--- app/components/Buttons/Button.tsx
+++ app/components/Buttons/Button.tsx
@@ -1,33 +1,44 @@
 import React from 'react'
 
-export const Button = ({ children }: { children: React.ReactNode }) => {
+const Button = ({ children, variant }: { children: React.ReactNode, variant?: 'default' | 'outlined' | 'ghost' | 'soft' }) => {
+  let buttonClass = "py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg";
+  let textColorClass = "text-white";
+  let bgColorClass = "bg-sky-600";
+  let borderColorClass = "border-transparent";
+  let hoverBgColorClass = "hover:bg-sky-700";
+  let hoverTextColorClass = "hover:text-white";
+
+  switch (variant) {
+    case 'outlined':
+      textColorClass = "text-sky-600";
+      bgColorClass = "bg-transparent";
+      borderColorClass = "border-sky-600";
+      hoverBgColorClass = "hover:bg-sky-100";
+      hoverTextColorClass = "hover:text-sky-600";
+      break;
+    case 'ghost':
+      textColorClass = "text-sky-600";
+      bgColorClass = "bg-transparent";
+      hoverBgColorClass = "hover:bg-sky-100";
+      borderColorClass = "border-0";
+      hoverTextColorClass = "hover:text-sky-800";
+      break;
+    case 'soft':
+      bgColorClass = "bg-sky-100";
+      textColorClass = "text-sky-800";
+      hoverBgColorClass = "hover:bg-sky-200";
+      borderColorClass = "border-0";
+      hoverTextColorClass = "dark:hover:bg-sky-900 dark:text-sky-400";
+      break;
+    default:
+      break;
+  }
+
   return (
-    <button type="button" className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-sky-600 text-white hover:bg-sky-700 disabled:opacity-50 disabled:pointer-events-none">
+    <button type="button" className={`${buttonClass} ${borderColorClass} ${bgColorClass} ${textColorClass} ${hoverBgColorClass} ${hoverTextColorClass} rounded-lg border hover:border-sky-600 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-neutral-400 dark:hover:text-sky-500 dark:hover:border-sky-600`}>
       {children}
     </button>
   )
 }
 
-export const OutlinedButton = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <button type="button" className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-sky-600 text-sky-600 hover:border-sky-600 hover:bg-sky-100 hover:text-sky-600 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-neutral-400 dark:hover:text-sky-500 dark:hover:border-sky-600">
-      {children}
-    </button>
-  )
-}
-
-export const GhostButton = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <button type="button" className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-sky-600 hover:bg-sky-100 hover:text-sky-800 disabled:opacity-50 disabled:pointer-events-none dark:text-sky-500 dark:hover:bg-sky-800/30 dark:hover:text-sky-400">
-      {children}
-    </button>
-  )
-}
-
-export const SoftButton = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <button type="button" className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-sky-100 text-sky-800 hover:bg-sky-200 disabled:opacity-50 disabled:pointer-events-none dark:hover:bg-sky-900 dark:text-sky-400">
-      {children}
-    </button>
-  )
-}
+export default Button
