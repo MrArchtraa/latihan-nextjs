2d945ff08d70f5ceb6f01cc977d51c208541d1ef
Andhika R. <archtraa.creative@gmail.com>
2024-04-22 18:18:57 +0700

    testing card

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,31 @@ import Button from "./components/Buttons/Button";
 import OutlinedButton from "./components/Buttons/OutlinedButton";
 import CancelButton from "./components/Buttons/CancelButton";
 import OutlinedCancelButton from "./components/Buttons/OutlinedCancelButton";
+
 import Card from "./components/Cards/Card";
+import CardHeader from "./components/Cards/CardHeader";
+import CardBody from "./components/Cards/CardBody";
+import CardFooter from "./components/Cards/CardFooter";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>
-        Press me
-      </Button>
-      <OutlinedButton>
-        Press me
-      </OutlinedButton>
-      <CancelButton>
-        Press me
-      </CancelButton>
-      <OutlinedCancelButton>
-        Press me
-      </OutlinedCancelButton>
       <Card>
-        Hello
+        <CardHeader>
+          <h3 className="text-lg font-bold">Simple Card</h3>
+        </CardHeader>
+        <CardBody>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a hendrerit ipsum.
+            Pellentesque consectetur odio diam, sit amet dapibus turpis dapibus non.
+            Curabitur laoreet euismod bibendum.
+          </p>
+        </CardBody>
+        <CardFooter>
+          <Button>
+            Press me
+          </Button>
+        </CardFooter>
       </Card>
     </main>
   );
